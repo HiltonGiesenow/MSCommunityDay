3496c69e534709effe801899e1d157221e761342
Hilton Giesenow <hilton@giesenow.com>
2018-02-12 13:23:35 +0200

    adding slides, one script

---
+++ 2018/Site Creation/Demos/Enable-PartnerPack.ps1
@@ -0,0 +1,28 @@
+﻿import-module SharePointPnPPowerShellOnline
+
+$cred = Get-Credential
+
+Connect-PnPOnline https://spgurus.sharepoint.com/teams/2013test/TemplateSubSite -Credentials $cred
+
+Apply-PnPProvisioningTemplate -Path C:\Development\Sandbox\DL\PnP\scripts\PnP-Partner-Pack-Overrides.xml
+
+Set-PnPPropertyBagValue -Key "_PnP_PartnerPack_Overrides_Enabled" -Value "true"
+
+
+Connect-PnPOnline https://spgurus.sharepoint.com/teams/2013test/TemplateSubSite -Credentials $cred
+
+Enable-PnPResponsiveUI -InfrastructureSiteUrl https://spgurus.sharepoint.com/sites/PnP-Partner-Pack-Infrastructure
+
+Connect-PnPOnline https://spgurus.sharepoint.com/teams/2013test/UsingTemplate -Credentials $cred
+
+
+PropertyBag_TemplateInfo
+
+
+Connect-PnPOnline https://spgurus.sharepoint.com/sites/ModernTeamSite1 -Credentials $cred
+
+Get-PnPProvisioningTemplate -Out c:\temp\modernSite.pnp
+
+Connect-PnPOnline https://spgurus.sharepoint.com/sites/ModernTeamSite2 -Credentials $cred
+
+Apply-PnPProvisioningTemplate -path c:\temp\modernSite.pnp
